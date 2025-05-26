160017dda6f5a2dae70073aa051e9b2799f2fd95
ramakrishna-K7 <ramakrishnakannekanti868@gmail.com>
2025-05-26 15:12:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.end('Hello from CI/CD Pipeline!');
+  res.end('Hello from CI/CD Pipeline! namaste');
 });
 
 server.listen(port, () => {
